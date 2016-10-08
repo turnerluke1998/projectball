c90fe8a6d654409d007d4fc5a0fa8e9dc6875f82
Luke Turner <turnerluke1998@aol.co.uk>
2016-10-08 23:23:28 +0100

    Added Basic Movement and Jump Controls
    Controls:
    A or Left to move Left
    D or Right to move Right
    W or Space to jump

---
+++ Assets/playerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jump;
+    float moveVelocity;
+    bool grounded = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 3;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump control
+        if (Input.GetKeyDown(KeyCode.Space) ||
+            Input.GetKeyDown(KeyCode.W))
+        {
+            if (grounded) {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            }
+        }
+        moveVelocity = 0;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+            moveVelocity = -moveSpeed; //move it left
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveVelocity = moveSpeed; //move it right
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+    }
+    //checks if it is on the ground or not
+    void OnTriggerEnter2D() {
+        grounded = true;
+    }
+    void OnTriggerExit2D() {
+        grounded = false;
+    }
+}
