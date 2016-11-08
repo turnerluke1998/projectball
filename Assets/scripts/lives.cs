569b7237a57665245256acae88a191d071ab658c
turnerluke1998 <turnerluke1998@aol.co.uk>
2016-11-08 14:56:11 +0000

    Big Update!
    - implemented damage indicator when the player collides with an enemy
    (it shows but does not vanish after)
    - implemented working moving platform (next steps: allow the player to
    move with the platform)
    - added moving Stone,nes enemy (needs to be animated)
    - fixed a few bugs

--- Assets/scripts/lives.cs
+++ Assets/scripts/lives.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
+using System.Threading;
 
 public class lives : MonoBehaviour
 {
@@ -8,52 +10,54 @@ public class lives : MonoBehaviour
     public GameObject Heart1;
     public GameObject Heart2;
     public GameObject Heart3;
+    public GameObject damage;
+    int milsecs = 2000;
+
 
 
     // Use this for initialization
     void Start()
     {
+        damage.SetActive(false);
     }
 
     void Update()
     {
+
     }
 
     void OnCollisionStay2D(Collision2D coll)
+        //destroys a heart each time you touch an enemy and then if all 3 hearts are gone, you die.
     {
         if (coll.gameObject.tag == "Player")
         {
+        }
+        if (livesCount == 0)
+        {
+            Destroy(Heart1);
             livesCount++;
             Destroy(gameObject);
+            damage.SetActive(true);
+            //damage.SetActive(false);
         }
         if (livesCount == 1)
         {
-            Destroy(Heart1);
+            Destroy(Heart2);
+            livesCount++;
+            Destroy(gameObject);
+            damage.SetActive(true);
+            //damage.SetActive(false);
         }
         if (livesCount == 2)
         {
             Destroy(Heart3);
-        }
-        if (livesCount == 3)
-        {
-            Destroy(Heart2);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            livesCount++;
+            Destroy(gameObject);
+            damage.SetActive(true);
+            //damage.SetActive(false);
             livesCount = 0;
             Coin.coinCount = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
-
-        /*
-                if (livesCount == 2)
-                livesCount = livesCount - 1;
-            Destroy(Heart2);
-            Destroy(gameObject);
-            if (livesCount == 1)
-                livesCount = livesCount - 1;
-            Destroy(Heart3);
-            Destroy(gameObject);
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        */
-//if (lives == 2) display "heart" at(some coordinates) display "heart" at(some coordinates);
-//if (lives == 3) display "heart" at(some coordinates) display "heart" at(some coordinates) display "heart" at(some coordinates)
--- Assets/scripts/quit.cs
+++ Assets/scripts/quit.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class quit : MonoBehaviour
+public class quitcontroller : MonoBehaviour
 {
     void Update()
     {
