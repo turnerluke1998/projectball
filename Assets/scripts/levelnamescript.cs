4f11d22622d904b0bfb4506af57e3a0f55767db6
turnerluke1998 <turnerluke1998@aol.co.uk>
2016-11-16 13:01:24 +0000

    Updating Files

---
+++ Assets/scripts/levelnamescript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelnamescript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey)
+            Destroy(this.gameObject);
+    }
+}
