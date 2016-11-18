369f1b83c71977c0c7f019ae582a862cc62f6fdb
Luke Turner <turnerluke1998@aol.co.uk>
2016-11-18 20:40:20 +0000

    Small fixes to the win screen and coin display counter

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -10,7 +10,7 @@ public class Coin : MonoBehaviour {
     {
         string coinText = "" + coinCount;
         GUI.skin.box.fontSize = 100;
-        GUI.Box(new Rect(Screen.width - 1650, 90, 160, 130), coinText);
+        GUI.Box(new Rect(Screen.width - 1650, 90, 190, 130), coinText);
     }
 
     // Use this for initialization
